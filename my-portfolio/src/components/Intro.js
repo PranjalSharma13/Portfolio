4ebb5424f93f958c8ede79c461356025e8473fc0
PranjalSharma13 <1SI17CS077@sit.ac.in>
2024-05-18 15:17:21 -0400

    language Change

--- my-portfolio/src/components/Intro.js
+++ my-portfolio/src/components/Intro.js
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import {useTranslation} from "react-i18next";
 import frontpic from './../frontpic.mp4';
 import './Intro.css';
+
 function  Intro (){
+  const [t,i18n]=useTranslation("global")
     const handleMoreAboutMe = () => {
         alert('More About Me button clicked!');
       };
     
     return (<>
+
       <div className="video-container">
      <video className="background-video"loop autoPlay muted>
         <source src={frontpic} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="overlay-text">
-      Hello,I am <div className='name'>Pranjal Sharma</div><br/>A full-stack web developer.
+    {t("home.intro1")}<div className='name'> Pranjal Sharma</div><br/>{t("home.intro2")}
       <button className="more-about-me-button" onClick={handleMoreAboutMe}>
-        More About Me
+      {t("home.moreButton")}
       </button>
       </div>
-    
+      
       </div>
     </>);
 }
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
+import global_en from './translations/en/global.json';
+import global_es from './translations/es/global.json';
+import global_hi from './translations/hi/global.json';
+import global_ne from './translations/ne/global.json';
+
+i18next.use(initReactI18next).init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+    hi: {
+      global: global_hi,
+    },
+    ne: {
+      global: global_ne,
+    },
+  },
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
